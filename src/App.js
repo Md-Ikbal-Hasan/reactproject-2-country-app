4f10d1559d14b8088e9600baa25c91a6dab9bb23
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-08-04 09:40:39 +0600

    data fetch successfullu from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+const url = "https://restcountries.com/v3.1/all";
 
 const App = () => {
+
+  const [isLoading, setIsloading] = useState(true);
+  const [error, setError] = useState(null);
+  const [countries, setCountries] = useState([]);
+
+  //fetching data from api...............
+  const fetchData = async (url) => {
+    setIsloading(true);
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setCountries(data);
+      setIsloading(false);
+      setError(null);
+      console.log(data);
+    }
+    catch (error) {
+      setIsloading(false);
+      setError(error);
+
+    }
+
+
+  }
+
+
+
+  useEffect(() => {
+    fetchData(url);
+
+  }, [])
+
+
+
+
   return (
     <div>
       <h1>Ikbal</h1>
